af9a9370d7050c62cf9ca9268f1759c4588360e5
Tadeu Espindola Palermo <tepalermo@stefanini.com>
2022-10-08 21:06:27 -0300

    teste de remoção de tarefa

--- src/test/java/br/com/cobradev/tasks/functional/TasksTest.java
+++ src/test/java/br/com/cobradev/tasks/functional/TasksTest.java
@@ -116,5 +116,27 @@ public class TasksTest {
 		}
 	}
 	
+	@Test
+    public void deveRemoverTarefaComSucesso() throws MalformedURLException {
+        WebDriver driver = acessarAplicacao();
+        try {
+            // inserir tarefa
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("task")).sendKeys("Teste via selenium");
+            driver.findElement(By.id("dueDate")).sendKeys("30/10/2030");
+            driver.findElement(By.id("saveButton")).click();
+            String message = driver.findElement(By.id("message")).getText();
+            Assert.assertEquals("Success!", message);
+            
+            // remover tarefa
+            driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
+            message = driver.findElement(By.id("message")).getText();
+            Assert.assertEquals("Success!", message);
+        } finally {
+            // fechar browser
+            driver.quit();
+        }
+    }
+	
 
 }
